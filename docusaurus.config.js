8fbe22da4e5d78dec5e81d5010122cbe5b79d5df
everettjf <everettjf@live.com>
2021-03-04 10:20:15 +0800

    update

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,12 +37,12 @@ module.exports = {
           title: 'Docs',
           items: [
             {
-              label: 'Style Guide',
+              label: 'Get Started',
               to: 'docs/',
             },
             {
-              label: 'Second Doc',
-              to: 'docs/doc2/',
+              label: 'Documents',
+              to: 'docs/',
             },
           ],
         },
